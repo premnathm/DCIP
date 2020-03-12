f56f6a2b994a3f29561e593a5354ff143c7da3cc
Abbas <abbasalis@dckap.com>
2020-03-12 22:59:53 +0530

    Leaderboard and history

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, session, url_for, render_template, request, jsonify
 from flask_mysqldb import MySQL
 from flask_hashing import Hashing
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -44,10 +45,21 @@ def index():
 @app.route('/leaderboard', methods=['GET', 'POST'])
 def leaderboard():
     cur = mysql.connection.cursor()
-    query = "SELECT id,name,designation,total_points FROM dcip_employees"
+    query = "SELECT id,name,designation,total_points,employeeid FROM dcip_employees where total_points > 0"
     cur.execute(query)
     employeesdata = cur.fetchall()
     return render_template('leaderboard.html',result=employeesdata)
+
+@app.route('/karma-history/<int:emp_id>')
+def karmahistory(emp_id):
+    cur = mysql.connection.cursor()
+    query = ('SELECT it.item_name, tr.asssigned_point FROM dcip_transactions as tr left join dcip_employees as em on tr.employee_id = em.id '
+    'left join dcip_items as it on tr.item_id = it.id where tr.asssigned_point > 0 and em.id = %s order by tr.created_at desc') 
+    param = (emp_id,)
+    cur.execute(query,param)
+    karmahistory = cur.fetchall()
+    # return jsonify(karmahistory)
+    return render_template('karmahistory.html',result=karmahistory)    
         
 if __name__ == '__main__':
     app.run()
